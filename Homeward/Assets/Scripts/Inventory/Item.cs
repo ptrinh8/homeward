91309d1da150f08b60f157c83685dce09074f6c8
daujiv <d.vaid@hotmail.com>
2014-12-01 10:20:14 -0800

    12/01/14 Mining_Refining_Inventory working
    Minerals properly appear in inventory
    Inventory dropping works
    Refining works

--- Homeward/Assets/Scripts/Inventory/Item.cs
+++ Homeward/Assets/Scripts/Inventory/Item.cs
@@ -26,16 +26,24 @@ public class Item
 	public string itemDescription;
 	public ItemType itemType;
 	public Texture2D itemIcon;
+    public Texture2D itemIconEmpty;
+    public Texture2D itemIconReplace;
 	public GameObject itemObject;
+
+    [HideInInspector]
+    public int value;
 	
-	public Item(string itemName, int itemID, string itemDescription, ItemType itemType, Texture2D itemIcon, GameObject itemObject)
+	public Item(string itemName, int itemID, string itemDescription, ItemType itemType, Texture2D itemIcon, Texture2D itemIconEmpty, Texture2D itemIconReplace, GameObject itemObject, int value)
 	{
 		this.itemName = itemName;
 		this.itemID = itemID;
 		this.itemDescription = itemDescription;
 		this.itemType = itemType;
 		this.itemIcon = itemIcon;
+        this.itemIconEmpty = itemIconEmpty;
+        this.itemIconReplace = itemIconReplace;
 		this.itemObject = itemObject;
+        this.value = value;
 	}
 
 	public Item()
