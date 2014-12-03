08ecb767ac1210818299f5e5f2dda81ab4634d86
tsato1 <tsato1@ucsc.edu>
2014-12-03 13:48:58 -0800

    12/03/14 Day/night edited and title screen added

---
+++ Homeward/Assets/Scripts/Instruction/InstructionDatabase.cs
@@ -0,0 +1,33 @@
+﻿// ==================================================================================
+// <file="InstructionDatabase.cs" product="Homeward">
+// <date>2014-11-11</date>
+// <summary>Create a Database for items in the inventory</summary>
+// ==================================================================================
+
+#region Header Files
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+#endregion
+
+[System.Serializable]
+public class InstructionDatabase : MonoBehaviour
+{
+
+    public List<InstructionItem> instItems = new List<InstructionItem>();
+
+    public InstructionItem GetItem(int id)
+    {
+        foreach (InstructionItem instItem in instItems)
+        {
+            if (instItem.itemID == id)
+            {
+                return instItem;
+            }
+        }
+        return null;
+    }
+
+}
