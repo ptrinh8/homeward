1d4ebb4faebefa1b12c03a34fca127e73b110011
ptrinh8 <ptrinh8@gmail.com>
2015-01-21 13:06:50 -0800

    1/21/15 fixed audio implementation

--- Homeward/Assets/Scripts/Audio/AudioController.cs
+++ Homeward/Assets/Scripts/Audio/AudioController.cs
@@ -20,7 +20,7 @@ public class AudioController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		stemTrigger.setValue(stemTriggerValue);
+		//stemTrigger.setValue(25f);
 	
 	}
 
