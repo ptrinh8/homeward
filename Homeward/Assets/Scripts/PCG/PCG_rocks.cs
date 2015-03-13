5e71547f02bf62faf6045cae319a8070b17a4cdb
daujiv <d.vaid@hotmail.com>
2015-03-12 20:54:22 -0700

    +PCG (updated) - takes 1/2 seconds to load.

--- Homeward/Assets/Scripts/PCG/PCG_rocks.cs
+++ Homeward/Assets/Scripts/PCG/PCG_rocks.cs
@@ -6,13 +6,26 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class PCG_rocks : MonoBehaviour 
 {
+    private PCG_Rand rand = new PCG_Rand();
+
     public GameObject rock;
     private int[] _x = new int[620];
     private int[] _y = new int[620];
     private bool doOnce = false;
+
+    public bool triggerEntered = false;
+    public bool addRemoveTiles = false;
+    public bool initialFunctionCall = false;
+    public bool moveTrigger = false;
+
+    public bool TriggerEntered_Property { get { return triggerEntered; } set { triggerEntered = value; } }
+    public bool AddRemoveTiles_Property { get { return addRemoveTiles; } set { addRemoveTiles = value; } }
+    public bool InitialFunctionCall_Property { get { return initialFunctionCall; } set { initialFunctionCall = value; } }
+    public bool MoveTrigger_Property { get { return moveTrigger; } set { moveTrigger = value; } }
 
     int jj = 0;
     int ii = 1;
@@ -47,21 +60,81 @@ public class PCG_rocks : MonoBehaviour
 
     }
 
+    void AddRemoveTiles()
+    {
+        GameObject mainPlayer = GameObject.Find("MainPlayer");
+        Vector2 mainPlayerPos = mainPlayer.transform.position;
+
+        if (triggerEntered == true || initialFunctionCall == false)
+        {
+            initialFunctionCall = true;
+            if (!addRemoveTiles)
+            {
+                addRemoveTiles = true;
+                triggerEntered = false;
+                moveTrigger = false;
+
+                GameObject[] gameObjectTile = GameObject.FindGameObjectsWithTag("RocksOnScreen");
+                List<GameObject> gameObjectTileList = gameObjectTile.ToList();
+                List<GameObject> finalList = gameObjectTileList.Distinct().ToList();
+
+
+                for (int i = 0; i < 620; i++)
+                {
+                    var distance = Vector2.Distance(new Vector2(_x[i], _y[i]), mainPlayerPos);
+                    if (distance < 20.0F)
+                    {
+
+                        rocks[i] = Instantiate(rock, new Vector3(_x[i], _y[i], 0), transform.rotation) as GameObject;
+                        rocks[i].tag = "RocksOnScreen";
+                        rocks[i].name = "Rocks " + i;
+
+                        if (rand.tempRndNosRock1[i] < 0.25F)
+                        {
+                        }
+                        else if ((rand.tempRndNosRock1[i] < 0.50F) && (rand.tempRndNosRock1[i] > 0.25F))
+                        {
+                        }
+                        else if (rand.tempRndNosRock1[i] < 0.75F && (rand.tempRndNosRock1[i] > 0.50F))
+                        {
+                        }
+                        else if (rand.tempRndNosRock1[i] > 0.75F)
+                        {
+                        }
+
+                        foreach (GameObject item in finalList)
+                        {
+                            if (item.name == rocks[i].name)
+                            {
+                                Destroy(rocks[i]);
+                            }
+                        }
+
+                    }
+                }
+
+                GameObject[] allTexturesMyFriend = GameObject.FindGameObjectsWithTag("RocksOnScreen");
+                foreach (GameObject texture in allTexturesMyFriend)
+                {
+                    var distance2 = Vector2.Distance(texture.transform.position, mainPlayerPos);
+                    if (distance2 > 20.0F)
+                    {
+                        GameObject.Destroy(texture);
+                    }
+                }
+            }
+        }
+    }
+
     void Start() 
     {
+        rand = FindObjectOfType(typeof(PCG_Rand)) as PCG_Rand;
         RndNosGeneration();
 	
 	}
 	
 	void Update () 
     {
-        if (!doOnce)
-        {
-            doOnce = true;
-            for (int i = 0; i < 620; i++)
-            {
-                rocks[i] = Instantiate(rock, new Vector3(_x[i], _y[i], 0), transform.rotation) as GameObject;
-            }
-        }
+        AddRemoveTiles();
 	}
 }
