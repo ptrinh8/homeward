d7724b9405dbab4f062f60e53d7eaf49c7c2a092
ptrinh8 <ptrinh8@gmail.com>
2015-05-18 23:18:14 -0700

    5/18/15 still fixing stuff

--- Homeward/Assets/Scripts/PlayerController/PlayerController.cs
+++ Homeward/Assets/Scripts/PlayerController/PlayerController.cs
@@ -487,7 +487,7 @@ public class PlayerController : MonoBehaviour
         moduleDescription.GetComponent<CanvasGroup>().alpha = 0;
 
         songLength = 120f;
-        songSilenceLength = 120f;
+        songSilenceLength = 5f;
         songTimer = 0f;
         songSilenceTimer = 0f;
         isSongQueued = false;
@@ -525,10 +525,10 @@ public class PlayerController : MonoBehaviour
 		playerParticleSystem.renderer.sortingLayerName = "GameplayLayer";
 		playerParticleSystem.loop = false;
 
-        rainParticleSystem = GameObject.Find("RAIN").GetComponent<ParticleSystem>();
-        rainParticleSystem.renderer.sortingLayerName = "GameplayLayer";
-        rainParticleSystem.loop = true;
-        rainParticleSystem.Play();
+        //rainParticleSystem = GameObject.Find("RAIN").GetComponent<ParticleSystem>();
+        //rainParticleSystem.renderer.sortingLayerName = "GameplayLayer";
+        //rainParticleSystem.loop = true;
+        //rainParticleSystem.Play();
 
 		anim = GetComponent<Animator>();
     }
@@ -676,11 +676,11 @@ public class PlayerController : MonoBehaviour
             {
 				if (CentralControl.isInside == true)
 				{
-					rainParticleSystem.Stop();
+					//rainParticleSystem.Stop();
 				}
 				else
 				{
-					rainParticleSystem.Play ();
+					//rainParticleSystem.Play ();
 				}
                 if (isSleeping == false)
                 {
@@ -1404,7 +1404,7 @@ public class PlayerController : MonoBehaviour
             {
                 songSelected = Random.Range(1, 5);
                 songLength = Random.Range(100f, 200f);
-                songSilenceLength = Random.Range(100f, 200f);
+                songSilenceLength = Random.Range(0f, 10f);
                 isSongQueued = true;
                 firstSongPlayed = true;
             }
